77d986bbce00ba1d0090f6489bfe1b04a0cfdd35
unknown <ggomesnascimento123@gmail.com>
2024-06-16 16:55:31 -0300

    Adicionando API Git

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,2 +1,17 @@
-let MY_TOKEN = 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
 
+async function PreparePage() {
+    let userData = await (await fetch("https://api.github.com/users/IsaqueCN")).json();
+    let userRepos = await (await fetch(userData.repos_url)).json();
+
+    let link = userdata.html_url;
+    let loginName = userData.login;
+    let name = userData.name;
+    let followersCount = userData.followers;
+    let bio = userdata.bio;
+    let location = userData.location;
+    let website = userData.blog;
+    let reposCount = userData.public_repos;
+
+}
+
+PreparePage();
