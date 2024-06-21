9565cac1bdc54b9916308dfca0e920ca57b8123d
unknown <ggomesnascimento123@gmail.com>
2024-06-21 17:05:02 -0300

    Finalizando projeto

--- public/assets/scripts/mainPage.js
+++ public/assets/scripts/mainPage.js
@@ -40,7 +40,7 @@ async function UpdatePage() {
     let userRepos = await (await fetch(userData.repos_url, GETHeader)).json()
     let userSocialMedia = await (await fetch("https://api.github.com/users/IsaqueCN/social_accounts")).json()
     let Profilelink = userData.html_url
-    
+
     LoginName.textContent = userData.login
     LoginName.href = Profilelink
     PerfilImg.src = userData.avatar_url
